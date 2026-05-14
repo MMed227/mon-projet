9ebf71b46aac1da8f18156246acd89a7861ab370
Mohamed <mohamed@example.com>
2026-05-14 22:26:50 +0100

    fix: remove BOM from prisma schema, add .env, replace ESLint config, renormalize EOL, run prisma generate/migrate/seed

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+﻿module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2024,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next/core-web-vitals'],
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-undef': 'off',
+  },
+  ignorePatterns: ['node_modules/', '.next/', 'dist/', 'prisma/', '*.log'],
+};
